32de915aa38094bf75b2c3ce64ae0c448192cb48
Timóteo Freires de Souza Bentes <bentestimoteo@gmail.com>
2022-10-21 16:21:57 -0400

    teste9

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const conexao = require("./db");
+const cors = require("cors");
 
 // Conexão com Banco de Dados
 conexao();
 
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 // rotas
 const motorista = require("./routes/motoristaRoutes");
-app.use("/bencar", motorista);
+app.use("/", motorista);
 
 // Porta de uso
 const PORT = process.env.PORT || 3000;
